e09ee5ee145f2fb426adf65848e8ad8207ee254f
William Proudfoot <bill.proudfoot@btinternet.com>
2018-02-21 14:11:44 +0000

    Set up Siri and tested okay

--- Tinker/ViewController.swift
+++ Tinker/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import Intents
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        INPreferences.requestSiriAuthorization{ (status) in
+            if status == INSiriAuthorizationStatus.authorized {
+                print("We can use Siri!")
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
